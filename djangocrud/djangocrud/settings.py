c34340e9944b47bcd0a8c8328c9e27b339b0227e
jose miguuel cano vilcapaza <jcanov@unsa.edu.pe>
2021-07-30 19:18:11 -0500

    Instalando django-cors-headers y realizando modificaciones al settings de django

--- djangocrud/djangocrud/settings.py
+++ djangocrud/djangocrud/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-sfas5e%nzbt&mepn9)q2l4v*wc^*sexg7gq)cof-dx(d^_vje!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ORIGINS_WHITELIST = [
+    "localhost:4200"
+]
 
 # Application definition
 
@@ -38,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
