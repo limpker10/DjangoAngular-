19e4974db054d738791ff88c313384fd10ffad32
jose miguuel cano vilcapaza <jcanov@unsa.edu.pe>
2021-07-30 19:12:08 -0500

    Agregando funcion para la realizar la consulta a django

--- crud/src/app/api.service.ts
+++ crud/src/app/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHandler, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +10,7 @@ export class ApiService {
 
   baseurl = "http://localhost:8000"
   httpHeaders = new HttpHeaders({'Content-type':'application/json'})
+
   constructor(private http: HttpClient) { }
 
   getAllMovies(): Observable<any>{
--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   movies = [{title:'titanic'},{title : 'avatar'}]
+  constructor (private api:ApiService){
+    this.getMovies();
+  }
+  getMovies=()=>{
+    this.api.getAllMovies().subscribe(
+      data =>{
+        this.movies = data
+      },
+      error =>{
+        console.log(error)
+      }
+    )
+  }
 }
